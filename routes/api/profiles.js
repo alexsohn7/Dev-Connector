852ca77af7905ba3cd327b17aec92f946875d471
alexsohn7 <alexsohn7@gmail.com>
2019-01-02 10:09:58 -0800

    users can now register but need to fix the gravatar.  Hashed password

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -2,9 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
+// @route GET api/profiles/test
+// @desc Tests profiles route
+// @access Public
 router.get("/test", (req, res) =>
   res.json({
-    msg: "Profile works"
+    msg: "Profiles works"
   })
 );
 

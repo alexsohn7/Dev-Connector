4add4b1b6d3a58ab16c079b537fbdb87923dbd3f
alexsohn7 <alexsohn7@gmail.com>
2019-01-02 13:51:44 -0800

    generating token with jwt

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://alex:alex123@ds147714-a0.mlab.com:47714,ds147714-a1.mlab.com:47714/dev-connector?replicaSet=rs-ds147714"
+    "mongodb://alex:alex123@ds147714-a0.mlab.com:47714,ds147714-a1.mlab.com:47714/dev-connector?replicaSet=rs-ds147714",
+  secretOrKey: "secret"
 };

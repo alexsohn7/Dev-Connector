cf1dcc53affde07ea7f71862892c659c790748cc
alexsohn7 <alexsohn7@gmail.com>
2019-01-03 06:48:51 -0800

    users can now delete experience by the id

--- validation/education.js
+++ validation/education.js
@@ -7,7 +7,7 @@ module.exports = function validateEducationInput(data) {
   data.school = !isEmpty(data.school) ? data.school : "";
   data.degree = !isEmpty(data.degree) ? data.degree : "";
   data.fieldofstudy = !isEmpty(data.fieldofstudy) ? data.fieldofstudy : "";
-  data.from = !isEmpty(data.from) ? data.from : "";
+  data.ls = !isEmpty(data.from) ? data.from : "";
 
   if (Validator.isEmpty(data.school)) {
     errors.school = "School field is required";

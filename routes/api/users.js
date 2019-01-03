2f9b99e83b5f01643e2f060c580765239ef685b8
alexsohn7 <alexsohn7@gmail.com>
2019-01-02 17:17:11 -0800

    login validation completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load Input Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load User model
 const User = require("../../models/User");
@@ -70,13 +71,21 @@ router.post("/register", (req, res) => {
 // @access Public
 
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Checking validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Find the user by the email
   User.findOne({ email: email }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(400).json(errors);
     }
 
     // Check Password
@@ -100,7 +109,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password is incorrect" });
+        errors.password = "Password is incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
